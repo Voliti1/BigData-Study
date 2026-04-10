02564be552edb34ac4bb1706f2d006c955b22863
Voliti1 <sjwnick2010@gmail.com>
2026-04-10 17:14:39 +0900

    update chap 5 code

---
+++ 실습코드/import pandas as pd.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/iris.csv")
+fig, axes = plt.subplots(nrows = 2, ncols = 2)
+
+df['Petal_Length'].plot.hist(ax = axes[0, 0])
+df['Petal_Length'].plot.box(ax = axes[0, 1])
+
+fd = df['Species'].value_counts()
+fd.plot.pie(ax = axes[1, 0])
+fd.plot.barh(ax = axes[1, 1])
+
+fig.suptitle('Multiple Graph Example', fontsize = 14)
+plt.show()
